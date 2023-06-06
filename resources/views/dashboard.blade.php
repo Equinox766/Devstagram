5d509987814df57999f89080a3477a5a53872c94
Equinox766 <equinox766@gmail.com>
2023-06-06 12:04:00 -0400

    Mostrando las publicaciones de los usuarios en su perfil!

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -27,4 +27,23 @@
             </div>
         </div>
     </div>
+    <section class="container mx-auto mt-10">
+        <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
+        @if($posts->count() > 0)
+        <div class="grid md:grid-cols-2 gap-6 lg:grid-cols-3 xl:grid-cols-4">
+            @foreach($posts as $post)
+                <div>
+                    <a>
+                        <img src="{{ asset('uploads').'/'.$post->imagen }}" alt="imagen del {{ $post->titulo }}">
+                    </a>
+                </div>
+            @endforeach
+        </div>
+            <div class="my-10">
+                {{ $posts->links('pagination::tailwind') }}
+            </div>
+        @else
+            <p class="text-gray-600 uppercase text-sm text-center font-bold">NO hay posts</p>
+        @endif
+    </section>
 @endsection

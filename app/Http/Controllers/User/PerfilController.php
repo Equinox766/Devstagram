0e686ac340eee12f48678288361acf0165fe7cc4
Equinox766 <equinox766@gmail.com>
2023-06-07 11:09:17 -0400

    Editar perfil de usuario!

---
+++ app/Http/Controllers/User/PerfilController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
+
+class PerfilController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('user.index');
+    }
+
+    public function store(Request $request)
+    {
+        $request = $request->merge(['username' => Str::slug($request->username)]);
+
+        $this->validate($request, [
+            'username' => ['required','max:20','unique:users,username,'.auth()->user()->id,'min:3', 'not_in:editar-perfil,crear-cuenta,login,posts,posts/create'],
+        ]);
+        if ($request->imagen)
+        {
+            $imagen = $request->file('imagen');
+            $nombreImagen = Str::uuid(). "." . $imagen->extension();
+            $imagenServidor =  Image::make($imagen);
+            $imagenServidor->fit(1000,1000);
+            $imagenPath = public_path('users'). '/' .$nombreImagen;
+            $imagenServidor->save($imagenPath);
+        }
+
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request->username;
+        $usuario->imagen = $nombreImagen ?? auth()->user()->imagen ?? null;
+        $usuario->save();
+
+        return redirect()->route('posts.index', $usuario->username);
+
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Post\ComentarioController;
-use App\Http\Controllers\Post\ImagenController;
+use App\Http\Controllers\User\FollowerController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Post\LikeController;
 use App\Http\Controllers\Post\PostController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Post\ImagenController;
+use App\Http\Controllers\User\PerfilController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Post\ComentarioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,24 +25,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('principal');
 })->name('principal');
+// REGISTRO DEL USUARIO
 Route::get('/crear-cuenta', [RegisterController::class, 'index'])->name('register');
 Route::post('/crear-cuenta', [RegisterController::class, 'store']);
 
+//LOGIN Y LOGOUT
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
 
+//PERFIL DEL USUARIO
+Route::get('/editar-perfil', [PerfilController::class, 'index'])->name('perfil.index');
+Route::post('/editar-perfil', [PerfilController::class, 'store'])->name('perfil.store');
+
+// RUTAS DE POSTS
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/{user:username}/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
 
-Route::post('/{user:username}/posts/{post}', [ComentarioController::class, 'store'])->name('comentarios.store');
-
+// PUBLICAR IMAGENES
 Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.store');
 
 //Likes a las fotos
-
 Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
 Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
+
+
+// VARIABLES DE POSTS Y COMENTARIOS
+Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
+Route::get('/{user:username}/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::post('/{user:username}/posts/{post}', [ComentarioController::class, 'store'])->name('comentarios.store');
+Route::post('/{user:username}/follow', [FollowerController::class, 'index'])->name('follow.index');
+Route::delete('/{user:username}/unfollow', [FollowerController::class, 'destroy'])->name('follow.unfollow');
+
